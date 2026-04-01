05c7ab0784af2560ed74c1ae4aec3ccb4635faf5
evann8226-cmd <evann8226@gmail.com>
2026-04-01 14:49:57 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿siswa siswaObj = new siswa("Evan", "Evan@gmail.com", "12345", "siswa", "XPG2");
+guru guruObj = new guru("kuyang", "kuyang@gmail.com", "12345", "guru", "coding");
+
+
+class user
+{
+    public string Nama;
+    public string Email;
+    public string Pasword;
+    public string Role;
+
+    public user(string name, string email, string pasword,string role)
+    {
+        Nama = name;
+        Email = email;
+        Pasword = pasword;
+        Role = role;
+
+    }
+    
+    public void TampilanInfo()
+    {
+        Console.WriteLine("Menampilkan info user.....");
+        Console.WriteLine($"Nama:  {Nama}");
+        Console.WriteLine($"Role:  {Role}");
+    }
+
+}
+
+
+class siswa : user
+{
+    public string ClasName;
+
+    public siswa(string name, string email, string pasword, string role, string classname) : base(name, email, pasword, role)
+    {
+        ClasName = classname;
+    }
+
+    public void SubmitTugas()
+    {
+        Console.WriteLine($"Siswa {Nama} sudah mengumpulkan tugas");
+    }
+} 
+
+class guru : user
+{
+    public string Mapel;
+
+    public guru(string name, string email, string pasword, string role, string mapel) : base(name, email, pasword, role)
+    {
+        Mapel = mapel;
+    }
+
+    public void Nilai()
+    {
+        Console.WriteLine($"guru {Nama} sudah memberi nilai");
+    }
+}
